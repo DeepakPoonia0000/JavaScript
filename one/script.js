b8c818810dda435cbe5f798e713c3893d5ebc03a
DeepakPoonia0000 <dpoonia0000@gmail.com>
2024-02-01 13:56:10 +0530

    First

---
+++ one/script.js
@@ -0,0 +1,6 @@
+let a=4+5;
+document.getElementById("one").innerHTML = "<b>how are you</b>";
+document.querySelector(".scr1").innerHTML = "how are you " + a;
+console.log("deepak poonia" , 6+7);
+window.alert("hii how are you");
+document.
